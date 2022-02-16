0e96d89a82011e245cb0bf29e7ef9af9d85d31cc
Jordan-erifried <danerifried@gmail.com>
2022-02-16 17:35:19 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  const Quizzler({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
